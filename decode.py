c9f29f832ae4baaa8e8cf15d4e5e206ed2941926
singurty <singurty@gmail.com>
2021-06-12 21:45:11 +0545

    dictionary keys encoding fixed; replaced bencode.py with bcoding

--- decode.py
+++ decode.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
-import bencodepy
+from bcoding import bdecode
 
 torrent_file = 'ref.torrent'
 
-# the dictionary in .torrent file
-decoded = bencodepy.bdecode(open(torrent_file,'rb').read())
+# getting the dictionary in .torrent file
+with open(torrent_file, 'rb') as f:
+    torrent = bdecode(f)
+
+# debug
+print('announce: '+torrent['announce'])
+print(torrent['announce-list'])
+
+# keys in torrent: ['announce', 'announce-list', 'comment', 'created by', 'creation date', 'info']
+# keys in torrent['info']: ['files', 'name', 'piece length', 'pieces']

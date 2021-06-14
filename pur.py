a116e723868fdea005f80a2b1e786758a05fdbe6
singurty <singurty@gmail.com>
2021-06-14 20:48:37 +0545

    peers responding

--- pur.py
+++ pur.py
@@ -17,7 +17,7 @@ with open(torrent_file, 'rb') as f:
 
 info_binary = bencode(torrent['info'])
 info_hash = hashlib.sha1(info_binary).digest()
-peer_id = '-qB4170-t-FvepUJaWBf'.encode('utf-8')
+peer_id = b'-qB4170-t-FvepUJaWBf'
 total_length = 0
 for value in torrent['info']['files']:
    total_length += value['length']
@@ -39,6 +39,7 @@ def tracker_connect_udp():
         ip, port = socket.gethostbyname(parsed.hostname), parsed.port
         transaction_id = pack('>I', random.getrandbits(32))
         message = connection_id + action + transaction_id
+        print('connecting tracker {}'.format(tracker))
         response = send_message_udp(sock, (ip, port), message, action, transaction_id, len(message))
         if response:
             parsed_response = {
@@ -46,10 +47,11 @@ def tracker_connect_udp():
                 'transaction_id': unpack('>I', response[4:8])[0],
                 'connection_id': unpack('>Q', response[8:16])[0]
             }
-#            print(parsed_response)
+            print('announcing tracker {}'.format(tracker))
             peers += tracker_announce_udp(response[8:16], (ip, port), sock)
     peers = list(set(peers))
     print('number of peers: ' + str(len(peers)))
+    return peers
  
 
 def tracker_announce_udp(connection_id, connection, sock):
@@ -79,7 +81,7 @@ def tracker_announce_udp(connection_id, connection, sock):
         address_length = extra_bytes // 6
         addresses = []
         for offset in range(0, address_length):
-            ip = ipaddress.IPv4Address(response[20 + (6 * offset) : 24 + (6 * offset)])
+            ip = format(ipaddress.IPv4Address(response[20 + (6 * offset) : 24 + (6 * offset)]))
             port = unpack('>H', response[24 + (6 * offset) : 24 + (6 * offset) + 2])[0]
             addresses.append((ip, port))
         return addresses
@@ -103,4 +105,41 @@ def send_message_udp(sock, connection, message, action, transaction_id, full_siz
     return response
 
 
-tracker_connect_udp()
+def peers_connect(peers):
+    for peer in peers:
+        state = {
+            'am_choking': True,
+            'am_interested': False,
+            'peer_choking': True,
+            'peer_interested': False
+        }
+        print('connecting to peer {}'.format(peer[0]))
+        peer_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        peer_socket.settimeout(4)
+        try:
+            peer_socket.connect(peer)
+        except OSError as err:
+            print(err)
+            continue
+        # do handshake
+        pstrlen = 19
+        pstr = b'BitTorrent protocol'
+        reserved = b'\x00' * 8
+        handshake = pack(">B19s8s20s20s", pstrlen, pstr, reserved, info_hash, peer_id)
+        print('handshake size {}'.format(len(handshake)))
+        peer_socket.send(handshake)
+        response = b''
+        try:
+            while True:
+                buff = peer_socket.recv(4096)
+                if len(buff) == 0:
+                    break
+                response += buff
+        except socket.timeout:
+            pass
+        peer_socket.close()
+        print('response size {}'.format(len(response)))
+
+
+peers = tracker_connect_udp()
+peers_connect(peers)
